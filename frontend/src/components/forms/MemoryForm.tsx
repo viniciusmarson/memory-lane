14df47517848c9755efe4526457fd62afad0b17e
Vinicius Marson <vinicius.marson@coinbase.com>
2025-03-20 16:25:40 -0700

    disabled submit if errors

--- frontend/src/components/forms/MemoryForm.tsx
+++ frontend/src/components/forms/MemoryForm.tsx
@@ -147,6 +147,7 @@ export default function MemoryForm({
         <button
           type='submit'
           className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600'
+          disabled={!!errors}
         >
           {submitText}
         </button>
